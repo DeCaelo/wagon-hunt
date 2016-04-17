8698dffd389bd0cedd755596edb5d1271c9fdac7
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-04-17 21:36:33 +0200

    add model product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
 	def index
-		@products = ["kudoz", "uSlide", "Hubum" , "Roadstr"]
+		@products = Product.all
+	end
+
+	def show
+		@product = Product.find(params[:id])
 	end
 end
